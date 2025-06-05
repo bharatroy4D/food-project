0d8d7a413bec9baeef901af38d0c7cd51c4e0957
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-06-05 19:48:55 +0600

    home page loader off

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import LeftBar from './LeftBar/LeftBar';
 import RightBar from './RightBar/RightBar';
 import LeftSell from './LeftSell/LeftSell';
@@ -6,32 +5,7 @@ import RightSell from './RightSell/RightSell';
 import NewsLatter from '../../Components/NewsLatter/NewsLatter';
 import ExtraFoods from './ExtraFoods/ExtraFoods';
 
-const SimpleLoader = () => (
-    <div className="flex flex-col justify-center items-center">
-        <div className="flex space-x-2">
-            <span className="w-4 h-4 bg-blue-600 rounded-full animate-bounce delay-150"></span>
-            <span className="w-4 h-4 bg-blue-600 rounded-full animate-bounce delay-300"></span>
-            <span className="w-4 h-4 bg-blue-600 rounded-full animate-bounce"></span>
-        </div>
-        <p className="mt-4 text-gray-700 font-medium">Loading ...</p>
-    </div>
-);
-
 const Home = () => {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const timer = setTimeout(() => setLoading(false), 2500);
-        return () => clearTimeout(timer);
-    }, []);
-
-    if (loading) {
-        return (
-            <div className="fixed inset-0 flex justify-center items-center bg-white bg-opacity-95 z-50">
-                <SimpleLoader />
-            </div>
-        );
-    }
 
     return (
         <div className="mx-auto">
